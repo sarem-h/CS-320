91967b6bc6945c419c5d19c1866c27d6e783ef80
pir8bay <pirateytrewq@gmail.com>
2025-02-16 04:17:44 -0500

    file structure update

--- Assignment-03/MyCookBookApi/MyCookBookApi/obj/Release/net8.0/MyCookBookApi.AssemblyInfo.cs
+++ Assignment-03/MyCookBookApi/MyCookBookApi/obj/Release/net8.0/MyCookBookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCookBookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e9a8d6de48e59e14605e6dfb1279c4ee79f5a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfda65b8724b439eda8c1689ea221d6ad72a9e1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCookBookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCookBookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
